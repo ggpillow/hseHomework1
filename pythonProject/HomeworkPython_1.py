9964a4c6652feaa00593da346b0e0b75d6f017f2
ggpillow <ketnut3336@gmail.com>
2024-09-16 13:12:14 +1100

    Added code tasks

---
+++ pythonProject/HomeworkPython_1.py
@@ -0,0 +1,35 @@
+print("Задача 1")
+print("--------------------")
+
+year = int(input("Введите год: "))
+
+if year % 4 == 0 or year % 400 == 0:
+  print("Високосный год")
+else:
+    print("Обычный год")
+print("--------------------")
+print("Задача 2")
+print("--------------------")
+
+sum_last_numbers = 0
+sum_first_numbers = 0
+n = 0
+
+while n == 0:
+
+  ticket = input("Введите номер билета: ")
+
+  try:
+    if len(ticket) >= 6:
+      sum_first_numbers = int(ticket[0]) + int(ticket[1]) + int(ticket[2])
+      sum_last_numbers = int(ticket[-1]) + int(ticket[-2]) + int(ticket[-3])
+      if sum_first_numbers == sum_last_numbers:
+        print("Счастливый билет")
+        n += 1
+      else:
+        print("Несчастливый билет")
+        n += 1
+    else:
+      print("Введён некорректный номер билета!")
+  except ValueError:
+    print("Введён некорректный номер билета!")
